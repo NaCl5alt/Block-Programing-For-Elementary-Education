fb9fed4d624f2585d08630fc4419b87d64e8009a
SKM-5425 <m16076@g.metro-cit.ac.jp>
2020-10-19 10:12:02 +0900

    header.vue

--- front/src/components/header.vue
+++ front/src/components/header.vue
@@ -1,11 +1,18 @@
 <template>
-    <div class="hello">
-      <div id="app-104">
-      <button @click="user" type="submit" value="userpage" v-if="userlogin">userpage</button>
-      <button @click="question" type="button" value="questionadd" v-else-if="adminlogin">questionadd</button>
-      </div>
+    <div class="header">
+      <h1>logo</h1>
+      <span v-if="userlogin==true">
+        <button @click="mypage" type="button" value="mypage" >マイページ</button>
+        <button @click="logout" type="submit" value="logout" >ログアウト</button>
+      </span>
+      <span v-else-if="adminlogin==true">
+        <button @click="adminpage" type="button" value="adminpage" >管理者ページ</button>
+        <button @click="logout" type="submit" value="logout" >ログアウト</button>
+      </span>
+      <span v-else>
+        <button @click="login" type="submit" value="login" >ログイン</button>
+        <button @click="register" type="submit" value="register" >新規登録</button>
+      </span>
     </div>
-    <script>
 
-    </script>
 </template>
