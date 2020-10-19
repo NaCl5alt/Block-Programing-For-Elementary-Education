77a5e6d85b38a26b8bba541df96a0f588ae27425
SKM-5425 <m16076@g.metro-cit.ac.jp>
2020-10-19 10:00:47 +0900

    header.vue

--- front/src/components/header.vue
+++ front/src/components/header.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="hello">
-
-
+      <div id="app-104">
+      <button @click="user" type="submit" value="userpage" v-if="userlogin">userpage</button>
+      <button @click="question" type="button" value="questionadd" v-else-if="adminlogin">questionadd</button>
+      </div>
     </div>
+    <script>
+
+    </script>
 </template>
