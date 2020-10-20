c19dba593d7e5dd20b2248e421681af1330caee7
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-21 07:37:29 +0900

    update

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -4,8 +4,8 @@
 
 <template>
 <!-- HTML -->
-  <div class="top">
-    <h1>For Educations!</h1>
+  <div>
+    <!-- <h1>For Educations!</h1> -->
     <b-alert show>Default Alert</b-alert>
   </div>
 </template>
