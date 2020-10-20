dc3682068a063f89fa5f5bd4a58ff375daaf8655
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-21 07:35:24 +0900

    update

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -1,15 +1,12 @@
 <style scoped>
 
-h1 {
-  font-style: italic;
-}
-
 </style>
 
 <template>
 <!-- HTML -->
   <div class="top">
     <h1>For Educations!</h1>
+    <b-alert show>Default Alert</b-alert>
   </div>
 </template>
 
