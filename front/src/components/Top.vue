df8d53a04fc5fa50ea67b8277194e3d4135da8dd
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-20 12:45:24 +0900

    update

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -6,12 +6,6 @@
 <!-- HTML -->
   <div class="top">
     <h1>For Educations!</h1>
-
-    <b-form action="" name="login_form">
-      <b-form-group label="ユーザID">
-        <b-form-input type="text" placeholder="Enter your ID"></b-form-input>
-      </b-form-group>
-    </b-form>
   </div>
 </template>
 
