0533fe4484602156925cc8a07fe90dd9d7badbd3
S.S <m16072@g.metro-cit.ac.jp>
2020-10-20 12:08:38 +0900

    sample projectの追加

---
+++ sample/src/components/MyPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <h3 style="margin-left: -20px">商品</h3>
+    <div style="margin-left: 20px">
+      <li>
+        <a href="exhibit">出品する</a>
+      </li>
+      <li>
+        <a href="mypage/exhibit">出品中</a>
+      </li>
+      <li>
+        <a href="mypage/sold">売却済み</a>
+      </li>
+      <li>
+        <a href="mypage/purchased">購入済み</a>
+      </li>
+    </div>
+    <h3 style="margin-left: -20px">ユーザ</h3>
+    <div style="margin-left: 20px">
+      <li>
+        <a href="change/id">ユーザID変更</a>
+      </li>
+      <li>
+        <a href="change/name">ユーザ名変更</a>
+      </li>
+      <li>
+        <a href="change/passwd">パスワード変更</a>
+      </li>
+      <li>
+        <a href="mypage/delete">ユーザ削除</a>
+      </li>
+    </div>
+  </ul>
+</template>
