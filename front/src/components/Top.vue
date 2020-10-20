f35ea3f361b2ca9ba7820bd0becd8492255e7c21
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-20 10:10:22 +0900

    update

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -2,11 +2,6 @@
 <!-- HTML -->
   <div class="top">
     <h1>For Educations!</h1>
-     <b-button
-      size="lg"
-      variant="outline-primary">
-      Bootstrap Button
-    </b-button>
   </div>
 </template>
 
