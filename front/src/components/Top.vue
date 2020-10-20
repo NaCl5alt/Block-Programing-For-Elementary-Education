da5eaf39b8b705ba77a89f923c0fcbd24251a70e
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-20 12:42:14 +0900

    update

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -1,7 +1,17 @@
+<style scoped>
+
+</style>
+
 <template>
 <!-- HTML -->
   <div class="top">
     <h1>For Educations!</h1>
+
+    <form action="" name="login_form">
+      <b-form-group label="ユーザID">
+        <b-form-input type="text" placeholder="Enter your ID"></b-form-input>
+      </b-form-group>
+    </form>
   </div>
 </template>
 
@@ -9,9 +19,3 @@
 export default {
 }
 </script>
-
-<style scoped>
-.top {
-  background-color: cyan;
-}
-</style>
