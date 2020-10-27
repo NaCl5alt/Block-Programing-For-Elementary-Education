0fb6d2cd3d47e160775f239e72db09560553b333
saijo24 <m16073@g.metro-cit.ac.jp>
2020-10-28 08:28:23 +0900

    いいlinterないっすか？

--- front/src/components/Top.vue
+++ front/src/components/Top.vue
@@ -1,27 +1,30 @@
 <style scoped>
-
 h1 {
   font-style: italic;
   text-align: center;
 }
-
 </style>
 
 <template>
   <div id="body-div">
-
     <b-container fluid>
-
       <b-row>
         <!-- アプリタイトル -->
         <b-col sm="7">
-          <h1>For Educations!</h1>
+          <b-jumbotron
+            header="For Educations!"
+            lead="Programming Beginner"
+          ></b-jumbotron>
+          <!-- <h1>For Educations!</h1> -->
+
           <div>
             <p class="h2">開発用</p>
             <b-list-group>
               <b-list-group-item to="/login">ログイン画面へ</b-list-group-item>
               <b-list-group-item to="/regist">新規登録画面へ</b-list-group-item>
-              <b-list-group-item to="/question">問題一覧画面へ</b-list-group-item>
+              <b-list-group-item to="/question"
+                >問題一覧画面へ</b-list-group-item
+              >
               <b-list-group-item to="/admin">管理者画面へ</b-list-group-item>
             </b-list-group>
           </div>
@@ -32,41 +35,38 @@ h1 {
           <div id="form-div">
             <b-card bg-variant="light">
               <h2>ログイン</h2>
-              <b-form>
 
+              <b-form>
                 <b-form-group label="ユーザID" label-cols-sm="4">
-                  <b-form-input type="text"></b-form-input>
+                  <b-form-input type="text" name="user_id"></b-form-input>
                 </b-form-group>
 
                 <b-form-group label="パスワード" label-cols-sm="4">
-                  <b-form-input type="password"></b-form-input>
+                  <b-form-input type="password" name="password"></b-form-input>
                 </b-form-group>
 
                 <b-row>
                   <b-col></b-col>
                   <b-col>
-                    <b-button pill type="submit" variant="outline-primary">ログイン</b-button>
+                    <b-button pill type="submit" variant="outline-primary"
+                      >ログイン</b-button
+                    >
                   </b-col>
                   <b-col>
-                    <b-button pill type="reset" variant="outline-danger">リセット</b-button>
+                    <b-button pill type="reset" variant="outline-danger"
+                      >リセット</b-button
+                    >
                   </b-col>
                 </b-row>
-
               </b-form>
             </b-card>
-
           </div>
-
         </b-col>
-
       </b-row>
-
     </b-container>
-
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
