b53904cfe68bd96c5a0624cfdaf5a0c9f6f10829
saijo24 <m16073@g.metro-cit.ac.jp>
2020-11-17 15:00:57 +0900

    update

---
+++ front/src/components/AdminQuestions.vue
@@ -0,0 +1,14 @@
+<style scoped>
+</style>
+
+<template>
+  <div>
+    <b-container fluid>
+      <h1>問題管理ページ</h1>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
