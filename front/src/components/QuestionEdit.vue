0cd107a0fa17fd036a8eff5b006776d1875ba4ce
saijo24 <m16073@g.metro-cit.ac.jp>
2020-11-17 15:31:34 +0900

    update

--- front/src/components/QuestionEdit.vue
+++ front/src/components/QuestionEdit.vue
@@ -5,6 +5,8 @@
   <div>
     <b-container fluid>
       <h1>編集</h1>
+      <h3>問題タイトル: {{ title }}</h3>
+      <!-- <b-form></b-form> -->
     </b-container>
   </div>
 </template>
@@ -12,13 +14,22 @@
 <script>
 // import axios from "axios";
 export default {
+  data() {
+    return {
+      title: "test",
+      content: "",
+      answer: "",
+    };
+  },
   methods: {
     window: (onload = function () {
       // axios
-      //   .get(
-      //     "http://localhost:8080/admin/question/${this.$route.params['id']}/edit"
-      //   )
-      //   .then((response) => {})
+      //   .get("http://localhost/question/${this.$route.params['id']}/detail")
+      //   .then((response) => {
+      //     this.title = response.title;
+      //     this.content = response.content;
+      //     this.answer = response.answer;
+      //   })
       //   .catch((err) => {
       //     console.log(err);
       //   });
