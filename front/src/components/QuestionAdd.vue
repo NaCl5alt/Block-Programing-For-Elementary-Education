689fc8afb29406e9a03acc96b7f6d8bf285fd45d
saijo24 <m16073@g.metro-cit.ac.jp>
2020-11-23 09:34:22 +0900

    update

---
+++ front/src/components/QuestionAdd.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>問題追加</h1>
+
+    <b-form>
+      <b-form-group
+        label="タイトル"
+        description="問題の内容を把握しやすいように命名しましょう。"
+      >
+        <b-form-input type="text" required></b-form-input>
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
