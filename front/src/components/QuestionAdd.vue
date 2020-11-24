979b1311eba38bdd0c75b8a78c8a102f0c4b139c
saijo24 <m16073@g.metro-cit.ac.jp>
2020-11-24 15:07:46 +0900

    update

--- front/src/components/QuestionAdd.vue
+++ front/src/components/QuestionAdd.vue
@@ -1,52 +1,61 @@
 <template>
   <div>
-    <h1>問題追加</h1>
+    <b-container>
+      <h1>問題追加</h1>
+      <b-form>
+        <b-form-group
+          label="タイトル"
+          description="問題の内容を把握しやすいように命名しましょう。"
+        >
+          <b-form-input type="text" required></b-form-input>
+        </b-form-group>
 
-    <b-form>
+        <b-form-group label="問題">
+          <b-form-input type="text" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="答え" description="">
+          <b-form-input type="text" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group label="ヒント" description="">
+          <b-form-input type="text" required></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-container>
+
+    <!-- <b-form>
       <b-form-group
         label="タイトル"
         description="問題の内容を把握しやすいように命名しましょう。"
       >
         <b-form-input type="text" required></b-form-input>
       </b-form-group>
-        <div id="app-2">
-          <input type="text" v-model="message">
-          <div class="error" v-if="error.require">必須項目です。</div>
-          <div class="error" v-if="error.tooLong">長すぎます。</div>
-        </div>
 
+      <div id="app-2">
+        <input type="text" v-model="message" />
+        <div class="error" v-if="error.require">必須項目です。</div>
+        <div class="error" v-if="error.tooLong">長すぎます。</div>
+      </div>
 
-      <b-form-group
-        label="問題"
-        
-      >
+      <b-form-group label="問題">
         <b-form-input type="text" required></b-form-input>
       </b-form-group>
 
-      <b-form-group
-        label="答え"
-        description=""
-      >
+      <b-form-group label="答え" description="">
         <b-form-input type="text" required></b-form-input>
       </b-form-group>
 
-      <b-form-group
-        label="ヒント"
-        description=""
-      >
+      <b-form-group label="ヒント" description="">
         <b-form-input type="text" required></b-form-input>
       </b-form-group>
-        <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-
-    
-
-
+    </b-form> -->
   </div>
-
-  
-
 </template>
 
 <script>
