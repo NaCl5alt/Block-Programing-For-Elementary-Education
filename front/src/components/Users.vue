d866eb7deb681c9d6ed11d421ec42defc5917ff6
saijo24 <m16073@g.metro-cit.ac.jp>
2020-11-24 15:30:50 +0900

    update

--- front/src/components/Users.vue
+++ front/src/components/Users.vue
@@ -5,10 +5,33 @@
   <div>
     <b-container>
       <h1>ユーザ一覧</h1>
+      <ul>
+        <li v-for="md in mainDatas" :key="md.id">{{ md.data }}</li>
+      </ul>
     </b-container>
   </div>
 </template>
 
 <script>
-export default {};
+import Axios from "axios";
+export default {
+  data() {
+    return {
+      mainDatas: [],
+    };
+  },
+  methods: {
+    mtFunc() {
+      Axios.get("/api/user/progress").then((res) => {
+        var buf = res.data;
+        for (let i = 0; i < buf.length(); i++) {
+          this.mainDatas = this.mainDatas.push({
+            id: i,
+            data: res.data["uid"],
+          });
+        }
+      });
+    },
+  },
+};
 </script>
