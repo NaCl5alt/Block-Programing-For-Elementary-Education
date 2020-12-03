17758033029686e4a070df573c52f422e771568c
saijo24 <m16073@g.metro-cit.ac.jp>
2020-12-03 18:16:13 +0900

    update: ほぼでけた

--- front/src/components/Questions.vue
+++ front/src/components/Questions.vue
@@ -3,8 +3,23 @@
     <b-container>
       <h2>
         問題一覧
-        <span style="font-size: 50%; color: gray"
+        <!-- <span style="font-size: 50%; color: gray"
           >(問題数: {{ end }}/{{ max }})</span
+        > -->
+        <b-icon
+          id="popover-1"
+          icon="exclamation-circle-fill"
+          variant="secondary"
+          font-scale="0.7"
+        ></b-icon>
+        <b-popover
+          target="popover-1"
+          triggers="hover"
+          placement="right"
+          title="Info"
+          >問題に<ruby
+            ><rb>挑戦</rb><rp>(</rp><rt>ちょうせん</rt><rp>)</rp></ruby
+          >しましょう！！</b-popover
         >
       </h2>
       <div>
@@ -21,37 +36,34 @@
           </b-thead>
           <b-tbody>
             <b-tr v-for="q in questions" :key="q.id">
-              <b-td class="text-right" v-if="q.progress" variant="success">{{
-                q.id
-              }}</b-td>
-              <b-td class="text-right" v-else>{{ q.id }}</b-td
-              ><b-td class="text-right" v-if="q.progress" variant="success">{{
-                q.title
-              }}</b-td>
-              <b-td class="text-right" v-else>{{ q.title }}</b-td>
+              <b-td class="text-right">{{ q.id }}</b-td>
+              <b-td class="text-right">{{ q.title }}</b-td>
               <b-td>
-                <b-button href="/question/1">トライ!</b-button>
+                <router-link
+                  v-if="q.progress"
+                  class="btn btn-info"
+                  :to="{ name: 'Question', params: { id: q.id } }"
+                  >トライ!</router-link
+                >
+                <router-link
+                  v-else
+                  class="btn btn-success"
+                  :to="{ name: 'Question', params: { id: q.id } }"
+                  ><b-icon icon="star"></b-icon> 完了</router-link
+                >
               </b-td>
             </b-tr>
           </b-tbody>
         </b-table-simple>
-        <!-- <div class="row">
-          <div class="col">No.</div>
-          <div class="col-8">問題名</div>
-        </div>
-        <div v-for="question in questions" :key="question.id" class="row">
-          <div class="col">
-            <i v-if="question.progress" class="far fa-check-square"></i>
-            <i v-else class="far fa-square"></i>
-            {{ question.id }}
-          </div>
-          <div class="col-8">
-            {{ question.title }}
-          </div>
-        </div> -->
-
         <infinite-loading @infinite="infiniteHandler" spinner="spiral">
-          <div slot="spinner">ロード中...</div>
+          <div slot="spinner">
+            <b-icon
+              icon="arrow-counterclockwise"
+              animation="spin-reverse"
+              font-scale="4"
+            ></b-icon
+            >ロード中...
+          </div>
           <div slot="no-more">もう登録されている問題がないよ</div>
           <div slot="no-results">登録されている問題がないよ</div>
         </infinite-loading>
@@ -67,9 +79,12 @@ export default {
   data() {
     return {
       questions: [],
+      fields: [
+        { key: "id", label: "No." },
+        { key: "title", label: "問題名" },
+      ],
       end: 0,
       max: 0,
-      pageNumber: 0,
     };
   },
   methods: {
@@ -91,7 +106,7 @@ export default {
         })
         .catch((error) => {
           console.log(error);
-          this.max = 10;
+          this.max = 500;
         });
     },
     async firstQuestion() {
@@ -111,6 +126,7 @@ export default {
       console.log(this.questions);
     },
     async getQuestions() {
+      var test = false;
       await Axios.get("/api/question/paging", {
         qid: this.end,
       })
@@ -121,22 +137,15 @@ export default {
           console.log(error);
           for (let i = this.end + 1; i <= this.end + 50; i++) {
             if (i <= this.max) {
+              test = Boolean(Math.round(Math.random()));
               this.questions = this.questions.concat({
                 id: i,
                 title: "[TEST] Title",
-                progress: true,
+                progress: test,
               });
             }
           }
         });
-
-      // this.questions.sort(function (a, b) {
-      //   if (a.id > b.id) {
-      //     return 1;
-      //   } else {
-      //     return -1;
-      //   }
-      // });
       this.end = this.questions.length;
       console.log(this.questions);
     },
