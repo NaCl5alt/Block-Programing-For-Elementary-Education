eda38ff1b88605db76818aa0a1e455818c3a2494
S.S <m16072@g.metro-cit.ac.jp>
2020-12-07 14:01:46 +0900

    err processing create

--- front/src/components/header.vue
+++ front/src/components/header.vue
@@ -1,71 +1,67 @@
 <template>
-
-<div id="header">
-  <div class="border border-info">
-    <div class="bg-info">
-    <div class="section1 text-left" style="float: left;">
-      <a href="/">
-      <img src="../assets/logo.png" height="50" width="50" alt="ロゴ">
-      </a>
-      </div>
-
-    <div class="section1 text-right" style="float: right;">
-
-      <span v-if='token!==null && admin!=="true"'>
-      <!--<b-button squared
-      size="lg"
-      variant="success"
-      style="width:200px; height:50px;"
-      href="/user"
-      >ユーザーページ
-      </b-button>-->
-      <b-button squared
-      size="lg"
-      variant="danger"
-      style="width:200px; height:50px;"
-      href="/logout"
-      >ログアウト
-      </b-button>
-      </span>
-      <span v-else-if='token!==null && admin == "true" '>
-      <b-button squared
-      size="lg"
-      variant="success"
-      style="width:200px; height:50px;"
-      href="/admin"
-      >管理者ページ
-      </b-button>
-      <b-button squared
-      size="lg"
-      variant="danger"
-      style="width:200px; height:50px;"
-      href="/logout"
-      >ログアウト
-      </b-button>
-      </span>
-      <span v-else>
-        <b-button squared
-        size="lg"
-        variant="success"
-        style="width:200px; height:50px;"
-        href="/login"
-        >ログイン
-        </b-button>
-        <b-button squared
-        size="lg"
-        variant="danger"
-        style="width:200px; height:50px;"
-        href="/regist"
-        >新規登録
-      </b-button>
-      </span>
-      </div>
-      <div class="section1 clear" style="clear: both;">
-      </div>
-
+  <div id="header">
+    <div class="border border-info">
+      <div class="bg-info">
+        <div class="section1 text-left" style="float: left;">
+          <a href="/">
+          <img src="../assets/logo.png" height="50" width="50" alt="ロゴ">
+          </a>
+        </div>
+        <div class="section1 text-right" style="float: right;">
+          <span v-if='token!==null && admin!=="true"'>
+            <b-button squared
+            size="lg"
+            variant="success"
+            style="width:200px; height:50px;"
+            href="/user"
+            >ユーザーページ
+            </b-button>
+            <b-button squared
+            size="lg"
+            variant="danger"
+            style="width:200px; height:50px;"
+            href="/logout"
+            >ログアウト
+            </b-button>
+          </span>
+          <span v-else-if='token!==null && admin == "true" '>
+            <b-button squared
+            size="lg"
+            variant="success"
+            style="width:200px; height:50px;"
+            href="/admin"
+            >管理者ページ
+            </b-button>
+            <b-button squared
+            size="lg"
+            variant="danger"
+            style="width:200px; height:50px;"
+            href="/logout"
+            >ログアウト
+            </b-button>
+          </span>
+          <span v-else>
+            <b-button squared
+            size="lg"
+            variant="success"
+            style="width:200px; height:50px;"
+            href="/login"
+            >ログイン
+            </b-button>
+            <b-button squared
+            size="lg"
+            variant="danger"
+            style="width:200px; height:50px;"
+            href="/regist"
+            >新規登録
+          </b-button>
+          </span>
+        </div>
+        <div class="section1 clear" style="clear: both;">
+        </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -86,16 +82,17 @@ export default {
         // console.log('status: ' + res.status)
         if (res.status === 200) {
           this.verify = true
-        }else if(res.status === 401){
+        }
+      }).catch(err => {
+        // console.log(err)
+        if(err.response.status === 401){
           this.verify = false
           this.tokenget()
-        }else {
+        } else {
           this.verify = false
           this.$cookies.remove('token')
           this.$cookies.remove('admin')
         }
-      }).catch(err => {
-        console.log(err)
       })
     },
     async tokenget(){
@@ -107,13 +104,12 @@ export default {
           this.$cookies.set('token', res.data.token)
           this.$cookies.set('admin', res.data.admin)
           this.verify = true
-        }else {
-          this.$cookies.remove('token')
-          this.$cookies.remove('admin')
-          this.verify = false
         }
-      }).catch(err => {
-        console.log(err)
+      }).catch(() => {
+        // console.log(err)
+        this.$cookies.remove('token')
+        this.$cookies.remove('admin')
+        this.verify = false
       })
     }
   },
