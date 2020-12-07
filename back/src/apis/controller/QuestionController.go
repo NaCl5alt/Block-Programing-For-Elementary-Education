2e59fde3ad8311b724639e30d32378a5815ba0c6
Vagrant Default User <junngt1106@gmail.com>
2020-12-07 14:01:38 +0000

    Add new feature
    問題情報の取得apiを追加。

--- back/src/apis/controller/QuestionController.go
+++ back/src/apis/controller/QuestionController.go
@@ -22,6 +22,17 @@ type CountResponse struct {
 	Count int `json:"count"`
 }
 
+type QuestionHint struct {
+	Hint string `json:"hint"`
+}
+
+type QuestionContents struct {
+	Id      int            `json:"qid"`
+	Title   string         `json:"title"`
+	Content string         `json:"content"`
+	Hints   []QuestionHint `json:"hints"`
+}
+
 type QuestionAnswer struct {
 	IsCorrect bool `json:"accept"`
 }
@@ -59,6 +70,49 @@ func (pc QuestionController) Get(c *gin.Context) {
 
 	c.JSON(http.StatusOK, adf)
 
+}
+
+func (pc QuestionController) Contents(c *gin.Context) {
+	tokenString := c.Request.Header.Get("Authorization")
+	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	_, err := auth.VerifyToken(tokenString)
+	if err != nil {
+		c.String(http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	db := db.GormConnect()
+	problem := model.Problem{}
+	hints := []model.Hint{}
+
+	u64, err := strconv.ParseUint(c.Param("id"), 10, 0)
+	problem.ID = uint(u64)
+
+	db.First(&problem)
+
+	db.Where(model.Hint{
+		Pro_Id: int(problem.ID),
+	}).Find(&hints)
+
+	res_hints := []QuestionHint{}
+
+	for _, h := range hints {
+		res_hints = append(res_hints, QuestionHint{
+			Hint: h.Hint,
+		})
+	}
+
+	fmt.Println("qid:" + c.Param("id"))
+
+	adf := QuestionContents{
+		Id:      int(problem.ID),
+		Title:   problem.Pro_Title,
+		Content: problem.Pro_Content,
+		Hints:   res_hints,
+	}
+
+	c.JSON(http.StatusOK, adf)
 }
 
 func (pc QuestionController) Answer(c *gin.Context) {
